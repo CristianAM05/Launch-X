87e66d5941e1ddcbaff2c1062308db6e880c070d
CristianAM05 <cmartinezg599@gmail.com>
2022-12-25 17:12:35 -0500

    Agregar Proyecto Calculadora

---
+++ BackEnd/JS Ejercicios/Calculadora/CALCULADORA.md
@@ -0,0 +1,34 @@
+# Proyecto Calculadora
+
+Realicé una calculadora con las siguientes funciones:
+
+- Suma
+
+- Resta
+
+- Multiplicación
+
+- División
+
+- Raíz cuadrada
+
+- Potencia al número que el usuario te diga
+
+- Residuo de una división
+
+- Borrar todos los datos de la calculadora
+
+Utilice un HTML para el cuerpo, JavaScript para las funcionalidades y CSS para decorar todo.
+
+
+### Programas:
+
+- [calculadora.html](https://github.com/CristianAM05/Launch-X/blob/main/BackEnd/JS%20Ejercicios/Calculadora/calculadora.html) 
+
+- [calculadora.js](https://github.com/CristianAM05/Launch-X/blob/main/BackEnd/JS%20Ejercicios/Calculadora/calculadora.js)
+
+- [calculadora.css](https://github.com/CristianAM05/Launch-X/blob/main/BackEnd/JS%20Ejercicios/Calculadora/calculadora.css)
+
+### Resultado obtenido:
+
+[![Calculadora.jpg](https://i.postimg.cc/65JSn52Y/Calculadora.jpg)](https://postimg.cc/qgwjdrG3)
--- BackEnd/JS Ejercicios/Calculadora/calculadora.js
+++ BackEnd/JS Ejercicios/Calculadora/calculadora.js
@@ -1,30 +1,32 @@
-let operacion = document.getElementById("screen");
-let aCalcular = '';
+let operacion = document.getElementById("screen");//Lo utilizo para mostrar las operaciones en la pantalla
+let aCalcular = '';//Inicializa variable para hacer calculos
 
 function escribe(index){
-    let botonScreen =document.querySelector(`#${index}`);
-    let botonResultado;
+    let botonScreen =document.querySelector(`#${index}`);//Obtiene que presiona el usuario
+    let botonResultado;//Ayuda a obtener la operacion a realizar
 
-    if (index==="borrar") {
+    if (index==="borrar") { //Este if valida las funciones de clear, igual y raiz cuadrada
         operacion.innerHTML = "";
         aCalcular='';
         botonResultado='';
+        let raiz = document.querySelector("#raiz");
+        raiz.innerHTML = "√("; //Para que vuelva a la normalidad la raiz con el clear
     } else if (index==="resultado") {
         operacion.innerHTML = eval(aCalcular);
         aCalcular = "(" + aCalcular + ")";
         botonResultado="";
     } else if (index==="raiz") {
-        if (botonScreen.innerHTML === "√("){
+        if (botonScreen.innerHTML === "√("){//Abre un parentesis con la raiz para calcular
             operacion.innerHTML += botonScreen.innerHTML;
             botonScreen.innerHTML = ")";
             botonResultado = "Math.sqrt("; 
-        } else {
+        } else {//si el parentesis esta abierto se cierra el parentesis
             operacion.innerHTML += botonScreen.innerHTML;
             botonResultado = botonScreen.innerHTML;
             botonScreen.innerHTML = "√(";}
         }
 
-    else{
+    else{//El else valida los numeros y el resto de operaciones
         if (index==="potencia") {
             botonScreen.innerHTML = "²";
             botonResultado = "**2";
@@ -35,7 +37,7 @@ function escribe(index){
         operacion.innerHTML += botonScreen.innerHTML;
     }
 
-    aCalcular += botonResultado;
+    aCalcular += botonResultado;//Almacena la operacion a realizar en el momento
     console.log(aCalcular);
     console.log("----");
 }
